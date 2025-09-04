f149e87f51ca5c0bc6f1e7779c1c44bde797288e
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-09-04 16:32:17 +0630

    [add] User crud - Jwt

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     },
     skip: [:registrations]
     
+    namespace :admin do
+      resources :users
+    end
+
     namespace :api do
       namespace :v1 do
         resources :posts do
