0c291758b529b24ef3d2a91f822fffadf33506d6
P-000306\Ye Zar Ni Tun <yezarnitun@metateammyanmar.com>
2025-09-04 12:02:55 +0630

    [add] Login/Logout - Jwt

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: {
+      message: 'Logged in successfully.',
+      user: resource,
+      token: request.env['warden-jwt_auth.token']
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if current_user
+      render json: { message: 'Logged out successfully.' }, status: :ok
+    else
+      render json: { message: 'User has no active session.' }, status: :unauthorized
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(
+  email: 'test@gmail.com',
+  password: '12345678',
+  password_confirmation: '12345678'
+)
